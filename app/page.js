36b1d50ef8c2ea566e6fda34ce75bb7e72bdbe78
dparmar <116805076+dparmar04@users.noreply.github.com>
2025-06-13 18:27:48 +0530

    icon

--- app/page.js
+++ app/page.js
@@ -4,12 +4,6 @@ import FilterSidebar from './components/FilterSidebar/FilterSidebar';
 import ProductList from './components/ProductList/ProductList';
 import Footer from './components/Footer/Footer';
 
-export const metadata = {
-  title: "Metta Muse | Handmade Artisans Marketplace",
-  description: "Discover unique, handmade artisan products curated by Metta Muse. Shop globally, sustainably and ethically.",
-  keywords: "artisan, handmade, sustainable, ethical fashion, global marketplace"
-};
-
 export default function Home() {
   return (
     <div>
